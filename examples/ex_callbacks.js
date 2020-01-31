2fb99ca1546f178da2b81822920302a0b88498a0
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2020-01-31 10:16:49 +0700

    Add files via upload

---
+++ examples/ex_callbacks.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// you need to copy chrome driver into directory from which you start this file
+// in order to successfully run the example
+
+const webdriver = require('selenium-webdriver');
+
+//const ProxyPlugin = require('selenium-chrome-proxy-plugin');
+const ProxyPlugin = require('../');
+
+
+function startWithProxy(config) {
+  //const chrome        = require('selenium-webdriver/chrome');
+  //const chromeOptions = new chrome.Options();
+
+  return new ProxyPlugin({
+    proxyConfig: config,
+    //chromeOptions: chromeOptions,
+  }, (err, plugin) => {
+    console.log('PLUGIN READY');
+
+    return new webdriver.Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(plugin.chromeOptions)
+      .build()
+      .then((driver) => plugin.cleanup()
+          .then(() => driver.get('http://whatismyip.host/'))
+          .then(() => console.log('DONE'))
+      )
+      ;
+  });
+}
+
+const proxies = require('./credentials.json');
+const proxy0 = proxies[0];
+
+startWithProxy({
+  host: proxy0.host,         // proxy host
+  port: proxy0.port,         // proxy port
+  username: proxy0.username, // proxy username
+  password: proxy0.password, // proxy password
+});
---
+++ examples/ex_promises.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// you need to copy chrome driver into directory from which you start this file
+// in order to successfully run the example
+
+const webdriver = require('selenium-webdriver');
+
+//const ProxyPlugin = require('selenium-chrome-proxy-plugin');
+const ProxyPlugin = require('../');
+
+
+function startWithProxy(config) {
+  //const chrome        = require('selenium-webdriver/chrome');
+  //const chromeOptions = new chrome.Options();
+
+  return new ProxyPlugin({
+    proxyConfig: config
+    //chromeOptions: chromeOptions,
+  })
+    .then((plugin) => {
+      console.log('PLUGIN READY');
+      return new webdriver.Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(plugin.chromeOptions)
+        .build()
+        .then((driver) => plugin.cleanup()
+            .then(() => driver.get('http://whatismyip.host/'))
+            .then(() => console.log('DONE'))
+        )
+        ;
+    })
+    .catch((err) => console.log('ERROR:', err))
+    ;
+}
+
+module.exports = startWithProxy;
+
+const proxies = require('./credentials.json');
+proxies.forEach(proxy => startWithProxy(proxy));
+
+// startWithProxy({ host: '<proxy_host>', port: '<proxy_port>', username: '<proxy_username>', password: '<proxy_password>' });
